56efb1bae4acdea0764c4d3bbd340d69e2b0e4a1
oscarf4 <oscarvedo97@gmail.com>
2022-02-06 18:22:38 -0500

    Implementado para triángulos isósceles y condicionales en cuadrado

--- figuras.js
+++ figuras.js
@@ -1,59 +1,79 @@
-//Código del cuadrado
-console.group('Cuadrados');
+//Variables para las figuras
+//cuadrado
+const inputCuadrado = document.getElementById('inputCuadrado');
+const resultadoCuadrado = document.getElementById('resultadoCuadrado');
+//Triángulo Isósceles
+const input1TrianguloIsosceles = document.getElementById('lado1TrianguloIsosceles');
+const input2TrianguloIsosceles = document.getElementById('lado2TrianguloIsosceles');
+const input3TrianguloIsosceles = document.getElementById('baseTrianguloIsosceles');
+const resultadoIsosceles = document.getElementById('resultadoIsoceles');
 
-function perimetroCuadrado(lado) {
-    return lado * 4;
+//Código del Cuadrado
+function calcularAreaCuadrado() {
+    const ladoCuadrado = inputCuadrado.value;
+
+    if (!!ladoCuadrado) {
+        const area = ladoCuadrado * ladoCuadrado;
+        resultadoCuadrado.innerHTML = `El 'área' es de <strong>${area}</strong>cm2`;
+    } else {
+        resultadoCuadrado.innerHTML = '';
+    }
 }
-
-function areaCuadrado(lado) {
-    return lado * lado;
-}
-
-console.groupEnd();
-
-//Código del triángulo
-console.group('Triangulos');
-
-function perimetroTriangulo(lado1, lado2, base) {
-    return lado1 + lado2 + base;
-}
-
-function areaTriangulo(base, altura) {
-    return (base * altura) / 2
-}
-
-console.groupEnd();
-
-//Código del círculo
-console.group('Circulos');
-
-function diametroCirculo(radio) {
-    return radio * 2;
-}
-
-function perimetroCirculo(radio) {
-    const diametro = diametroCirculo(radio);
-    return diametro * Math.PI;
-}
-
-function areaCirculo(radio) {
-    return Math.PI * Math.pow(radio, 2);
-}
-
-console.groupEnd();
-
-//Funciones para calcular perímetros y áreas
 
 function calcularPerimetroCuadrado() {
-    const input = document.getElementById('inputCuadrado');
-    const value = input.value;
-    const perimetro = perimetroCuadrado(value);
-    alert(perimetro);
+    const ladoCuadrado = inputCuadrado.value;
+
+    if (!!ladoCuadrado) {
+        const perimetro = ladoCuadrado * 4;
+        resultadoCuadrado.innerHTML = `El 'perímetro' es de <strong>${perimetro}</strong>cm2`;
+    } else {
+        resultadoCuadrado.innerHTML = '';
+    }
 }
 
-function calcularAreaCuadrado() {
-    const input = document.getElementById('inputCuadrado');
-    const value = input.value;
-    const area = areaCuadrado(value);
-    alert(area);
+// //Código del triángulo
+// function perimetroTriangulo(lado1, lado2, base) {
+//     return lado1 + lado2 + base;
+// }
+
+// function areaTriangulo(base, altura) {
+//     return (base * altura) / 2
+// }
+
+// //Código del círculo
+// function diametroCirculo(radio) {
+//     return radio * 2;
+// }
+
+// function perimetroCirculo(radio) {
+//     const diametro = diametroCirculo(radio);
+//     return diametro * Math.PI;
+// }
+
+// function areaCirculo(radio) {
+//     return Math.PI * Math.pow(radio, 2);
+// }
+
+//Código del Triangulo Isósceles
+function calcularAreaTrianguloIsosceles() {
+    lado1 = input1TrianguloIsosceles.value;
+    lado2 = input2TrianguloIsosceles.value;
+    base = input3TrianguloIsosceles.value;
+
+    if (lado1 === lado2 && lado1 !== base) {
+        const altura = Math.sqrt(lado1**2 - base**2/ 4);
+        const area = (base * altura) / 2;
+        resultadoIsosceles.innerHTML = `El 'area' es de <strong>${area}</strong>cm2`;
+    }
+}
+
+function calcularPerimetroTrianguloIsosceles() {
+    lado1 = Number(input1TrianguloIsosceles.value);
+    lado2 = Number(input2TrianguloIsosceles.value);
+    base = Number(input3TrianguloIsosceles.value);
+
+    if (lado1 === lado2 && lado1 !== base) {
+        const perimetro = lado1 + lado2 + base;
+        resultadoIsosceles.innerHTML = `El 'perímetro' es de <strong>${perimetro}</strong>cm2`;
+    }
 }
